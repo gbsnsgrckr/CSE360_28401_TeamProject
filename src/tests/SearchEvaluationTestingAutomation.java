41bbd82b12389b3e060c1dc1cc7b9c4f2716126b
Kyle <kyle116cobra@gmail.com>
2025-02-26 18:26:30 -0700

    fixed name of SearchEvaluationTestingAutomation.java

--- src/tests/SearchEvaluationTestingAutomation.java
+++ src/tests/SearchEvaluationTestingAutomation.java
@@ -7,7 +7,7 @@ import java.util.List;
 import application.*;
 import databasePart1.*;
 
-public class SearchTestingAutomation {
+public class SearchEvaluationTestingAutomation {
 	private static final DatabaseHelper databaseHelper = new DatabaseHelper();
 	static int numPassed = 0;
 	static int numFailed = 0;
