e731c1483bc8636b15c30925b4cd0d22f6010bd7
Kyle <kyle116cobra@gmail.com>
2025-02-28 14:23:12 -0700

    Added transparency to Inbox page

--- TeamProjectPhase2/src/application/Inbox.java
+++ TeamProjectPhase2/src/application/Inbox.java
@@ -8,16 +8,19 @@ import java.util.Optional;
 import databasePart1.DatabaseHelper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 
 /** 
  * The inbox class shows all user generated message that are related to the
@@ -32,6 +35,9 @@ public class Inbox {
     }
 
     public void show(Stage primaryStage) {
+    	double[] offsetX = { 0 };
+		double[] offsetY = { 0 };
+    	
         TableView<Message> table = new TableView<>();
         List<Message> messageList = new ArrayList<>();
 
@@ -187,17 +193,146 @@ public class Inbox {
         // Layout
         HBox hbox = new HBox(5, backButton);
         hbox.setAlignment(Pos.CENTER);
-        VBox vbox = new VBox(10, prompt, table, hbox);
-        vbox.setAlignment(Pos.CENTER);
-        vbox.setStyle("-fx-padding: 20;");
+        VBox layout = new VBox(10, prompt, table, hbox);
+        layout.setAlignment(Pos.CENTER);
+        
+        layout.setMinSize(1300, 900);
+		layout.setMaxSize(1300, 900);
+		layout.setStyle("-fx-padding: 20; -fx-background-color: derive(gray, 80%); -fx-background-radius: 100;"
+				+ "-fx-background-insets: 4; -fx-border-color: gray, gray, black;"
+				+ "-fx-border-width: 2, 2, 1; -fx-border-radius: 100, 100, 100;" + "-fx-border-insets: 0, 2, 4");
 
-        Scene scene = new Scene(vbox, 940, 500);
+		// Actions to allow window dragging
+		layout.setOnMousePressed(a -> {
+			offsetX[0] = a.getSceneX();
+			offsetY[0] = a.getSceneY();
+		});
+
+		// Actions to allow window dragging
+		layout.setOnMouseDragged(a -> {
+			primaryStage.setX(a.getScreenX() - offsetX[0]);
+			primaryStage.setY(a.getScreenY() - offsetY[0]);
+		});
+
+		// Container to hold the buttons and allow for click+drag
+		// Button to replace X close button for transparent background
+		Button closeButton = new Button("X");
+		closeButton.setStyle(
+				"-fx-background-color: transparent; -fx-background-insets: 0; -fx-border-color: black; -fx-text-fill: black; -fx-font-size: 12px;"
+						+ "-fx-font-weight: bold; -fx-padding: 0;");
+		closeButton.setMinSize(25, 25);
+		closeButton.setMaxSize(25, 25);
+
+		// Button to replace maximize button for transparent background
+		Button maxButton = new Button("🗖");
+		maxButton.setStyle(
+				"-fx-background-color: transparent; -fx-background-insets: 0; -fx-border-color: black; -fx-text-fill: black; -fx-font-size: 12px;"
+						+ "-fx-font-weight: bold; -fx-padding: 0;");
+		maxButton.setMinSize(25, 25);
+		maxButton.setMaxSize(25, 25);
+
+		// Button to replace minimize button for transparent background
+		Button minButton = new Button("_");
+		minButton.setStyle(
+				"-fx-background-color: transparent; -fx-background-insets: 0; -fx-border-color: black; -fx-text-fill: black; -fx-font-size: 12px;"
+						+ "-fx-font-weight: bold; -fx-padding: 0;");
+		minButton.setMinSize(25, 25);
+		minButton.setMaxSize(25, 25);
+
+		// Set onAction events for button
+		closeButton.setOnMouseEntered(a -> {
+			closeButton.setStyle(
+					"-fx-background-color: gray; -fx-background-insets: 0; -fx-border-color: black; -fx-text-fill: red; -fx-font-size: 12px;"
+							+ "-fx-font-weight: bold; -fx-padding: 0;");
+			closeButton.setMinSize(25, 25);
+			closeButton.setMaxSize(25, 25);
+		});
+
+		closeButton.setOnMouseExited(a -> {
+			closeButton.setStyle(
+					"-fx-background-color: transparent; -fx-background-insets: 0; -fx-border-color: black; -fx-text-fill: black; -fx-font-size: 12px;"
+							+ "-fx-font-weight: bold; -fx-padding: 0;");
+			closeButton.setMinSize(25, 25);
+			closeButton.setMaxSize(25, 25);
+		});
+
+		closeButton.setOnAction(a -> {
+			primaryStage.close();
+		});
+
+		// Set onAction events for button
+		maxButton.setOnMouseEntered(a -> {
+			maxButton.setStyle(
+					"-fx-background-color: gray; -fx-background-insets: 0; -fx-border-color: black; -fx-text-fill: red; -fx-font-size: 12px;"
+							+ "-fx-font-weight: bold; -fx-padding: 0;");
+			maxButton.setMinSize(25, 25);
+			maxButton.setMaxSize(25, 25);
+		});
+
+		maxButton.setOnMouseExited(a -> {
+			maxButton.setStyle(
+					"-fx-background-color: transparent; -fx-background-insets: 0; -fx-border-color: black; -fx-text-fill: black; -fx-font-size: 12px;"
+							+ "-fx-font-weight: bold; -fx-padding: 0;");
+			maxButton.setMinSize(25, 25);
+			maxButton.setMaxSize(25, 25);
+		});
+
+		maxButton.setOnAction(a -> {
+			primaryStage.setMaximized(!primaryStage.isMaximized());
+		});
+
+		// Set onAction events for button
+		minButton.setOnMouseEntered(a -> {
+			minButton.setStyle(
+					"-fx-background-color: gray; -fx-background-insets: 0; -fx-border-color: black; -fx-text-fill: red; -fx-font-size: 12px;"
+							+ "-fx-font-weight: bold; -fx-padding: 0;");
+			minButton.setMinSize(25, 25);
+			minButton.setMaxSize(25, 25);
+		});
+
+		minButton.setOnMouseExited(a -> {
+			minButton.setStyle(
+					"-fx-background-color: transparent; -fx-background-insets: 0; -fx-border-color: black; -fx-text-fill: black; -fx-font-size: 12px;"
+							+ "-fx-font-weight: bold; -fx-padding: 0;");
+			minButton.setMinSize(25, 25);
+			minButton.setMaxSize(25, 25);
+		});
+
+		// Event to minimize the window
+		minButton.setOnAction(a -> {
+			primaryStage.setIconified(true);
+		});
+
+		// Container to hold the three buttons min, max, and close
+		HBox buttonBar = new HBox(5, minButton, maxButton, closeButton);
+		buttonBar.setAlignment(Pos.TOP_RIGHT);
+		buttonBar.setPadding(new Insets(0));
+		buttonBar.setMaxHeight(27);
+		buttonBar.setMaxWidth(80);
+
+		// Spacer to push the titleBar to the top
+		Region spacer = new Region();
+		spacer.setMinHeight(26);
+		spacer.setMaxHeight(26);
+
+		VBox layoutBox = new VBox(spacer, layout);
+		layoutBox.setAlignment(Pos.CENTER);
+
+		// StackPane to control layout sizing
+		StackPane root = new StackPane(layoutBox, buttonBar);
+		root.setAlignment(buttonBar, Pos.TOP_RIGHT);
+		root.setStyle("-fx-background-color: transparent;");
+		root.setPadding(new Insets(0));
+
+        Scene scene = new Scene(root, 1300, 930);
+        scene.setFill(Color.TRANSPARENT);
 
         // Removes icon from title bar in alert window
         primaryStage.getIcons().clear();
 
         primaryStage.setScene(scene);
         primaryStage.setTitle("Inbox");
+        primaryStage.centerOnScreen();
         primaryStage.show();
     }
 }
--- TeamProjectPhase2/src/application/StudentHomePage.java
+++ TeamProjectPhase2/src/application/StudentHomePage.java
@@ -196,7 +196,7 @@ public class StudentHomePage {
 
 		Button askToBeAReviewer = new Button("Ask to be a reviewer");
 		askToBeAReviewer.setStyle(
-				"-fx-text-fill: black; -fx-font-weight: bold; -fx-border-color: black; -fx-border-width:  1;");
+				"-fx-text-fill: black; -fx-font-weight: bold; -fx-border-color: black; -fx-border-width:  1px;");
 
 		// Button to check inbox for private messages
 		Button inboxButton = new Button("Inbox (#)"); // TODO: NEED TO GET NUMBER OF UNREAD PRIVATE MESSAGES
@@ -280,57 +280,6 @@ public class StudentHomePage {
 				titleBar.setStyle("-fx-pref-height: 0; -fx-min-height: 0; -fx-max-height: 0;");
 			}
 		});
-
-		/*
-		 * Answer Database Table that we shouldn't need // Table display of the answer
-		 * database // Label to display title to user Label prompt3 = new
-		 * Label("Answer Database"); prompt3.
-		 * setStyle("-fx-text-fill: black; -fx-font-size: 16px; -fx-font-weight: bold;"
-		 * );
-		 * 
-		 * // Hbox to position the title HBox titleBox3 = new HBox(prompt3);
-		 * titleBox3.setAlignment(Pos.CENTER);
-		 * 
-		 * // Create table to display the answer database TableView<Answer> aTable = new
-		 * TableView<>(); aTable.
-		 * setStyle("-fx-text-fill: black; -fx-font-weight: bold; -fx-border-color: black; -fx-border-width:  1;"
-		 * ); aTable.setPrefWidth(600);
-		 * 
-		 * // if answers is null then initialize as an empty list if (answers == null) {
-		 * answers = new ArrayList<>(); }
-		 * 
-		 * // Create an observable list and assign it to the table
-		 * ObservableList<Answer> answerObservableList =
-		 * FXCollections.observableArrayList(answers);
-		 * aTable.setItems(answerObservableList);
-		 * 
-		 * // Create, assign, and associate values to table TableColumn<Answer, Integer>
-		 * idColumn2 = new TableColumn<>("Answer ID");
-		 * idColumn2.setCellValueFactory(data -> new
-		 * ReadOnlyObjectWrapper<>(data.getValue().getId()));
-		 * 
-		 * // Create a text column TableColumn<Answer, String> textColumn2 = new
-		 * TableColumn<>("Answer"); textColumn2.setCellValueFactory(new
-		 * PropertyValueFactory<>("text"));
-		 * 
-		 * // Create a userID column TableColumn<Answer, Integer> authorColumn2 = new
-		 * TableColumn<>("Author ID"); authorColumn2.setCellValueFactory(new
-		 * PropertyValueFactory<>("author"));
-		 * 
-		 * // Create a createOn column TableColumn<Answer, String> createdColumn2 = new
-		 * TableColumn<>("Created On"); createdColumn2.setCellValueFactory(new
-		 * PropertyValueFactory<>("createdOn"));
-		 * 
-		 * // Create an updatedOn column TableColumn<Answer, String> updatedColumn2 =
-		 * new TableColumn<>("Updated On"); updatedColumn2.setCellValueFactory(new
-		 * PropertyValueFactory<>("updatedOn"));
-		 * 
-		 * aTable.getColumns().addAll(idColumn2, textColumn2, authorColumn2,
-		 * createdColumn2, updatedColumn2);
-		 * 
-		 * // Container to hold the table VBox answerDB = new VBox(5, titleBox3,
-		 * aTable);
-		 */
 
 		// Label to display title to user
 		Label prompt5 = new Label("Details");
@@ -1227,24 +1176,26 @@ public class StudentHomePage {
 		
 		HBox manageReviewerButtonBox = new HBox(manageReviewersButton);
 		manageReviewerButtonBox.setAlignment(Pos.BOTTOM_RIGHT);
+		
+		HBox inboxButtonBox = new HBox(inboxButton);
+		inboxButtonBox.setAlignment(Pos.BOTTOM_LEFT);
 
 		HBox quitButtonBox = new HBox(quitButton);
 		quitButtonBox.setAlignment(Pos.BOTTOM_LEFT);
 		
 		HBox askToBeAReviewerBox = new HBox(askToBeAReviewer);
 		askToBeAReviewerBox.setAlignment(Pos.BOTTOM_CENTER);
-
-
-		HBox buttonBox1 = new HBox(10, quitButtonBox, reviewerButtonBox, manageReviewerButtonBox, askToBeAReviewerBox, inboxButton);
-		quitButton.setAlignment(Pos.BOTTOM_LEFT);
-		findReviewerButton.setAlignment(Pos.BOTTOM_RIGHT);
 		
 		Button viewUnresolvedBtn = createViewUnresolvedButton();
 		Button viewAllUnresolvedBtn = createViewAllUnresolvedButton();
 		
-		HBox buttonBox2 = new HBox(viewUnresolvedBtn, viewAllUnresolvedBtn);
+		HBox buttonBox2 = new HBox(10, viewUnresolvedBtn, viewAllUnresolvedBtn);
 
-		VBox vbox1 = new VBox(10, vbox, buttonBox1, buttonBox2);
+		HBox buttonBox1 = new HBox(10, quitButtonBox, reviewerButtonBox, manageReviewerButtonBox, inboxButtonBox, buttonBox2, askToBeAReviewerBox);
+		quitButton.setAlignment(Pos.BOTTOM_LEFT);
+		findReviewerButton.setAlignment(Pos.BOTTOM_RIGHT);		
+
+		VBox vbox1 = new VBox(10, vbox, buttonBox1);
 		vbox1.setAlignment(Pos.CENTER);
 
 		// Hide submitBox unit needed
@@ -1261,11 +1212,13 @@ public class StudentHomePage {
 
 		});
 		
-		inboxButton.setOnAction(a -> {
-		    Inbox inbox = new Inbox(databaseHelper);
-		    Stage inboxStage = new Stage();
-		    inboxStage.initStyle(StageStyle.DECORATED);
-		    inbox.show(inboxStage);
+		inboxButton.setOnAction(a -> {		    
+		 // Create a new stage in order to popup new window and keep this one
+		 Stage newStage = new Stage();
+		 newStage.initStyle(StageStyle.TRANSPARENT);
+
+		 new Inbox(databaseHelper).show(primaryStage);
+		    
 		});
 
 		questionCloseButton.setOnAction(a -> {
@@ -1790,6 +1743,7 @@ public class StudentHomePage {
 	    // Displays this stage to the user
 	}
 
+	// Recursive method to add related answers to resultsTable and to prevent duplicates
 	private List<Answer> addRelatedAnswers(int parentId, List<Answer> answers) {
 		try {
 			// Retrieve related answers
