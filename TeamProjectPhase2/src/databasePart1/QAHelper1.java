b78280efdc34ada498e6c7f7e07107e7ad153afc
dquasaric <dara.abdulgafoor@gmail.com>
2025-02-28 15:05:58 -0700

    Update QAHelper1.java

--- TeamProjectPhase2/src/databasePart1/QAHelper1.java
+++ TeamProjectPhase2/src/databasePart1/QAHelper1.java
@@ -180,6 +180,7 @@ public class QAHelper1 {
 			ResultSet newID = pstmt.getGeneratedKeys();
 			if (newID.next()) {
 				int answerID = newID.getInt(1);
+				answer.setId(answerID);
 				addRelationToQuestion(relatedID, answerID);
 			}
 
