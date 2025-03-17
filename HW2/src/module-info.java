ddc59135212a4c7baf0eebb67673c9f35bfb496a
Kyle <kyle116cobra@gmail.com>
2025-03-17 16:57:30 -0700

    Adding folders to start HW3

--- HW2/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module CSE360TeamProject {
-	requires javafx.controls;
-	requires java.sql;
-	requires javafx.base;
-	requires javafx.graphics;
-	
-	opens application to javafx.graphics, javafx.fxml, javafx.base;
-}
