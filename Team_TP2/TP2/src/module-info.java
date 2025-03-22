6610da34d8899d4e589f730979a2457863996f04
Kyle <kyle116cobra@gmail.com>
2025-03-22 16:01:14 -0700

    no message

--- Team_TP2/TP2/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module CSE360TeamProject {
-	requires javafx.controls;
-	requires java.sql;
-	requires javafx.base;
-	requires javafx.graphics;
-	requires junit;
-	
-	opens application to javafx.graphics, javafx.fxml, javafx.base;
-}
--- Team_TP3/TP3/src/module-info.java
+++ Team_TP3/TP3/src/module-info.java
@@ -1,9 +1,8 @@
-module CSE360TeamProject {
+module Team_TP3 {
 	requires javafx.controls;
 	requires java.sql;
 	requires javafx.base;
 	requires javafx.graphics;
-	requires junit;
 	
 	opens application to javafx.graphics, javafx.fxml, javafx.base;
 }
