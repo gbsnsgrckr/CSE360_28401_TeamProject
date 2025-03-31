d55187231acfa1bd24ed6f46a0c03e00503b34ba
Chris Espinal <cespina3@asu.edu>
2025-03-30 21:34:24 -0400

    Update QAHelper1.java
    Added getAllMessages() method for testing purposes

--- Team_TP3/TP3/src/databasePart1/QAHelper1.java
+++ Team_TP3/TP3/src/databasePart1/QAHelper1.java
@@ -1274,6 +1274,27 @@ public class QAHelper1 {
 		}
 	}
 
+	public List<Message> getAllMessages() throws SQLException {
+	    String query = "SELECT * FROM cse360message";
+	    List<Message> messages = new ArrayList<>();
+
+	    try (PreparedStatement pstmt = connection.prepareStatement(query)) {
+	        ResultSet rs = pstmt.executeQuery();
+
+	        while (rs.next()) {
+	            int messageID = rs.getInt("messageid");
+	            int senderID = rs.getInt("senderid");
+	            int recipientID = rs.getInt("recipientid");
+	            String subject = rs.getString("subject");
+	            String content = rs.getString("message");
+
+	            Message message = new Message(databaseHelper, messageID, senderID, recipientID, subject, content);
+	            messages.add(message);
+	        }
+	    }
+	    return messages;
+	}
+
 	public List<Message> retrieveMessagesByUserId(int id) throws SQLException {
 		String query = "SELECT * FROM cse360message WHERE recipientid = ?";
 		List<Message> messages = new ArrayList<>();
