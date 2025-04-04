aa1251d77db348de0f3830694323688dbdca77eb
Chris Espinal <cespina3@asu.edu>
2025-04-04 01:26:39 -0400

    Update PopulateMessageDatabase.java
    update to fix test

--- Team_TP3/TP3/src/tests/PopulateMessageDatabase.java
+++ Team_TP3/TP3/src/tests/PopulateMessageDatabase.java
@@ -30,19 +30,19 @@ public class PopulateMessageDatabase {
         }
     	
     	
-    	Message m1 = new Message(1337, 2, "Meeting Reminder", "Don't forget about our meeting at 3 PM today.");
-     	Message m2 = new Message(1337, 2, "Project Update", "I have pushed the latest changes to the repository.");
-     	Message m3 = new Message(6721, 2, "Bug Found", "There is a bug in the login system. Please check it.");
-        Message m4 = new Message(2894, 2, "Code Review", "Can you review my latest code submission?");
-        Message m5 = new Message(5050, 2, "Feedback Request", "I would love to hear your feedback on my project.");
-        Message m6 = new Message(9021, 2, "Team Coordination", "Let's sync up on our project deliverables.");
-        Message m7 = new Message(7142, 2, "Issue Resolved", "The issue you reported has been resolved.");
-        Message m8 =  new Message(404, 2, "System Update", "We've deployed a new system update.");
-        Message m9 = new Message(404, 2, "New Feature", "Check out the new messaging feature!");
-        Message m10 = new Message(404, 2, "Testing", "This is a test message for user 404.");
-        Message m11 = new Message(404, 2, "Follow-up", "Following up on our previous discussion.");
-        Message m12 = new Message(1, 2, "Welcome", "Welcome to the platform! Let us know if you need any help.");
-        List<Message> messages = new ArrayList<>(Arrays.asList(m1,m2,m3,m4,m5,m6,m7,m9,m10,m11));
+    	Message m1 = new Message(1, "q", 1337, 2, "Meeting Reminder", "Don't forget about our meeting at 3 PM today.");
+     	Message m2 = new Message(2, "a", 1337, 2, "Project Update", "I have pushed the latest changes to the repository.");
+     	Message m3 = new Message(3, "m", 6721, 2, "Bug Found", "There is a bug in the login system. Please check it.");
+        Message m4 = new Message(4, "r", 2894, 2, "Code Review", "Can you review my latest code submission?");
+        Message m5 = new Message(5, "q", 5050, 2, "Feedback Request", "I would love to hear your feedback on my project.");
+        Message m6 = new Message(6, "q", 9021, 2, "Team Coordination", "Let's sync up on our project deliverables.");
+        Message m7 = new Message(7, "a", 7142, 2, "Issue Resolved", "The issue you reported has been resolved.");
+        Message m8 = new Message(8, "a", 404, 2, "System Update", "We've deployed a new system update.");
+        Message m9 = new Message(9, "r", 404, 2, "New Feature", "Check out the new messaging feature!");
+        Message m10 = new Message(10, "r", 404, 2, "Testing", "This is a test message for user 404.");
+        Message m11 = new Message(11, "m", 404, 2, "Follow-up", "Following up on our previous discussion.");
+        Message m12 = new Message(12, "r", 1, 2, "Welcome", "Welcome to the platform! Let us know if you need any help.");
+        List<Message> messages = new ArrayList<>(Arrays.asList(m1,m2,m3,m4,m5,m6,m7,m8, m9,m10 ,m11, m12));
         
     	
     	
